e01e0f20e7e1a7c8e2e73fca242a3898225e738a
vvictor <faraideiro@yahoo.com>
2025-04-25 16:31:23 +0300

    changed duration column expected input

--- api/models/recipe.py
+++ api/models/recipe.py
@@ -7,8 +7,12 @@ class Recipe(db.Model):
 
     id = db.Column(db.Integer, primary_key= True)
     name = db.Column(db.String(100), nullable = False)
-    duration = db.Column(db.Integer, nullable = False)
+    duration = db.Column(db.String(50), nullable = False)
     pictures = db.Column(db.Text, nullable = False)
     instructions = db.Column(db.Text, nullable = False)
 
-    categories = db.relationship('Category', secondary=recipe_category, backref=db.backref('recipes', lazy='dynamic'))
+    categories = db.relationship(
+        'Category',
+        secondary=recipe_category,
+        backref=db.backref('recipes', lazy='dynamic')
+    )
