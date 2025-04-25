fb26d118723b0c58185043ffe24e229e1beb56bf
vvictor <faraideiro@yahoo.com>
2025-04-25 16:33:22 +0300

    made unit nullable

--- api/models/ingredient.py
+++ api/models/ingredient.py
@@ -6,7 +6,7 @@ class Ingredient(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    unit = db.Column(db.String(50), nullable=False)
+    unit = db.Column(db.String(50), nullable=True)
     quantity = db.Column(db.Float, nullable=False)
 
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
